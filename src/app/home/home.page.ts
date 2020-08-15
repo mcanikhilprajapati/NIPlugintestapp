654ac40522d84f2767e9b5028dd0a0624d7c987c
nikhil <mca.nikhilprajapati@gmail.com>
2020-08-15 13:18:55 +0530

    in

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+
+declare var cordova: any;
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+    constructor() {
+
+    }
+
+    showAlert() {
+       // alert('test');
+        cordova.plugins.makePayment((data) => {
+            console.log(data);
+        }, (err) => {
+            console.log(err);
+        });
+    }
 
 }
