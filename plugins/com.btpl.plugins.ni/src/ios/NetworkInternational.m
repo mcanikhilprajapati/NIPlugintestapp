3f585d038b882f98bbedce076baecfc5cd81e22c
nikhil <mca.nikhilprajapati@gmail.com>
2020-08-23 15:29:18 +0530

    updated ios

--- plugins/com.btpl.plugins.ni/src/ios/NetworkInternational.m
+++ plugins/com.btpl.plugins.ni/src/ios/NetworkInternational.m
@@ -15,12 +15,13 @@
 {
     CDVPluginResult* pluginResult = nil;
     NSString* echo = [command.arguments objectAtIndex:0];
-
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
+     NSLog(@"test");
+//
+//     if (echo != nil && [echo length] > 0) {
+//         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+//     } else {
+//         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+//     }
 
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
